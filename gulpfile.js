fd732a4e36ef33fd4d393ec5585332c5e58378b8
NedelchoPenev <silenci0@abv.bg>
2019-04-16 16:28:32 +0300

    main-page

--- gulpfile.js
+++ gulpfile.js
@@ -4,31 +4,32 @@ let gulp = require("gulp"),
 	autoprefixer = require("gulp-autoprefixer"),
 	exec = require("gulp-exec"),
 	browserSync = require('browser-sync').create(),
+	sass = require('gulp-sass'),
 	child = require("child_process");
 ;
 
-gulp.task("css", function() {
-	return gulp.src( '_assets/css/**/*.css' )
-		.pipe( autoprefixer() )
-		.pipe( gulp.dest( './docs/css/' ) )
-		.pipe( browserSync.stream({ match: '**/*.css' }) )
-	;
+gulp.task("scss", function () {
+	return gulp.src('_assets/scss/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer())
+		.pipe(gulp.dest('./docs/css/'))
+		.pipe(browserSync.stream({ match: '**/*.css' }));
 });
 
 
-gulp.task("jekyll", function() {
-	return child.spawn("bundle.bat", ["exec", "jekyll.bat", "build", '--incremental'], { stdio: "inherit" });
+gulp.task("jekyll", function () {
+	return child.spawn("bundle.bat", ["exec", "jekyll.bat", "build", '--incremental'], { stdio: "inherit", shell: true });
 });
 
-gulp.task("watch", function() {
+gulp.task("watch", function () {
 
 	browserSync.init({
 		server: {
-            baseDir: "./docs/"
+			baseDir: "./docs/"
 		}
 	});
 
-	gulp.watch( '_assets/css/**/*.css', gulp.series('css') );
+	gulp.watch('_assets/scss/**/*.scss', gulp.series('scss'));
 
 	gulp.watch(
 		[
@@ -36,15 +37,13 @@ gulp.task("watch", function() {
 			"./_includes/*.html",
 			"./_layouts/*.html",
 			"./_homeworks/**/*.html",
-			"./_topics/*.html",
 			'_homeworks/**/**/*.css'
 		]
-	).on('change', gulp.series('jekyll', 'css') );
+	).on('change', gulp.series('jekyll', 'scss'));
 
-	gulp.watch( 'docs/**/*.html' ).on('change', browserSync.reload );
-	gulp.watch( 'docs/homeworks/**/**/*.css' ).on('change', browserSync.reload );
-	gulp.watch( 'docs/**/*.js' ).on('change', browserSync.reload );
-
+	gulp.watch('docs/**/*.html').on('change', browserSync.reload);
+	gulp.watch('docs/homeworks/**/**/*.css').on('change', browserSync.reload);
+	gulp.watch('docs/**/*.js').on('change', browserSync.reload);
 });
 
-gulp.task("default", gulp.series('css','watch'));
+gulp.task("default", gulp.series('scss', 'watch'));
